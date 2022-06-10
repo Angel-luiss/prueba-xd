84388f6592df572261641326043e38e51c780958
Angel <Angel>
2022-06-10 02:44:01 -0600

    Initial Commit

---
+++ pru2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  pru2
+//
+//  Created by Angel Tucubal on 10/06/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
